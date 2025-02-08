50d18404be4d89409a8607737a6f7b21fb5d4be4
SummCoder <2404174187@qq.com>
2025-02-08 18:18:57 +0800

    调整脚本位置

---
+++ run-ftp.sh
@@ -0,0 +1,54 @@
+#!/bin/bash  
+
+# 定义下载地址和文件名  
+DOWNLOAD_URL="https://cangjie-lang.cn/v1/files/auth/downLoad?nsId=142267&fileName=Cangjie-0.53.13-linux_x64.tar.gz&objectKey=6719f1eb3af6947e3c6af327"  
+FILE_NAME="Cangjie-0.53.13-linux_x64.tar.gz"
+
+# 检查 cangjie 工具链是否已安装
+
+echo "确保 cangjie 工具链已安装..."  
+if ! command -v cjc -v &> /dev/null  
+then  
+    echo "cangjie工具链 未安装，尝试进行安装..."  
+    # 下载文件  
+    echo "Downloading Cangjie compiler..."  
+    curl -L -o "$FILE_NAME" "$DOWNLOAD_URL"
+
+    # 检查下载是否成功  
+    if [ $? -eq 0 ]; then  
+        echo "Download completed successfully."  
+    else  
+        echo "Download failed."  
+        exit 1  
+    fi
+
+    # 解压文件  
+    echo "Extracting $FILE_NAME..."  
+    tar -xvf "$FILE_NAME"  
+
+    # 检查解压是否成功  
+    if [ $? -eq 0 ]; then  
+        echo "Extraction completed successfully."  
+    else  
+        echo "Extraction failed."  
+        exit 1  
+    fi
+
+    # 检查 envsetup.sh 是否存在并进行 source  
+    if [[ -f "cangjie/envsetup.sh" ]]; then
+    	echo "envsetup.sh found!"  
+        source cangjie/envsetup.sh  
+    else  
+        echo "envsetup.sh not found!"  
+        exit 1  
+    fi
+
+fi
+
+# 编译ftp_server
+cjc ./cangjie-ftp/ftp_server.cj -o ftp_server
+
+echo "正在启动ftp服务器..."
+
+# 启动执行
+./ftp_server
